7288cececd19fb535caf935e1a1f776cf0d86c5c
Ruy Braga Filho <ruybragafilho@gmail.com>
2022-10-21 16:48:18 -0300

    inicio da implementacao do pathfinder

--- pathfinder.py
+++ pathfinder.py
@@ -1,1 +1,22 @@
-.and
+class Pathfinder:
+
+
+    def bfs( self ):
+        pass
+
+    
+    def ids( self ):
+        pass
+
+
+    def ucs( self ):
+        pass
+
+
+    def greedy( self ):
+        pass
+
+
+    def a_star( self ):
+        pass
+    
